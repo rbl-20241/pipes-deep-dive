a557fd012ea93b644b66c5f6e44576265585e89f
rbl-20241 <blom@casema.nl>
2025-09-29 15:57:14 +0200

    168

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -5,7 +5,12 @@ import { Pipe, PipeTransform } from "@angular/core";
     standalone: true
 })
 export class TemperaturePipe implements PipeTransform {
-    transform(value: string | number) {
+    transform(value: string | number | null, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
+
+        if (!value) {
+            return value;
+        }
+
         let val: number;
 
         if (typeof value === 'string') {
@@ -14,8 +19,24 @@ export class TemperaturePipe implements PipeTransform {
             val = value;
         }
 
-        const outputTemp = val * (9 / 5) + 32;
+        let outputTemp: number;
+        if (inputType === 'cel' && outputType === 'fah') {
+            outputTemp = val * (9 / 5) + 32;
+        } else if (inputType === 'fah' && outputType === 'cel') {
+            outputTemp = (val -32) * (5 / 9);
+        } else {
+            outputTemp = val;
+        }
 
-        return `${outputTemp} °F`;
+        let symbol: '°C' | '°F';
+
+        if (!outputType) {
+            symbol = inputType == 'cel' ? '°C' : '°F';
+        } else {
+            symbol = outputType == 'cel' ? '°C' : '°F';
+        }
+    
+
+        return `${outputTemp.toFixed(2)} ${symbol}`;
     }
 }
