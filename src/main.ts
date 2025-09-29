d62657e23fc42f310f2688551354600038cd4dd6
rbl-20241 <blom@casema.nl>
2025-09-29 14:50:17 +0200

    162

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent)
   .catch((err) => console.error(err));
